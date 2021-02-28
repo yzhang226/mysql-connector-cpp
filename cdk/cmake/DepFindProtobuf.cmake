67e262e8ce3297aff7cdf6c87fc74136cd197536
zhangying <zhangying@hujiang.com>
2021-02-28 17:36:03 +0800

    update: protobuf dep build

--- cdk/cmake/DepFindProtobuf.cmake
+++ cdk/cmake/DepFindProtobuf.cmake
@@ -50,13 +50,14 @@ endif()
 
 message(STATUS "Setting up Protobuf.")
 
-hunter_add_package(Protobuf)
-find_package(Protobuf CONFIG REQUIRED)
+#hunter_add_package(Protobuf)
+#find_package(Protobuf CONFIG REQUIRED)
 #target_link_libraries(... protobuf::libprotobuf)
 
-message("use hunter Protobuf")
+#message("use hunter Protobuf")
 
-return()
+## can not return directly, require function below
+#return()
 
 set(PB_SOURCE_DIR "${PROJECT_SOURCE_DIR}/protobuf")
 set(PB_BINARY_DIR "${PROJECT_BINARY_DIR}/protobuf")
